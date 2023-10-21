52288716feb94e39502eae0a2a8189cfe5a010a6
Mohammad <mohazar9170@gmail.com>
2023-10-21 20:27:52 +0330

    verbose + trans for Case model / completeion of Case model before first makeMG

--- cases/models.py
+++ cases/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.urls import reverse
+from django.core.validators import MaxValueValidator
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 class Case(models.Model):
     STATUS = [
@@ -6,21 +10,34 @@ class Case(models.Model):
         ('s', 'Soon'),
         ('e', 'Ended'),
     ]
-
-    title = models.CharField(max_length=300)
-    maker = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    capacity = models.PositiveIntegerField()
-    meter = models.PositiveIntegerField()
-    base_value = models.PositiveIntegerField()
-    buy_assurance = models.BooleanField(default=False)
-    guaranteed_gain = models.BooleanField(default=False)
-    guaranteed_gain_percent = models.PositiveIntegerField()
-    end_time = models.PositiveIntegerField()
-    description = models.TextField()
-    status = models.CharField(max_length=10, choices=STATUS)
+    title = models.CharField(max_length=300, verbose_name=_('title'))
+    maker = models.CharField(max_length=100, verbose_name=_('maker'))
+    city = models.CharField(max_length=100, verbose_name=_('city'))
+    region = models.CharField(max_length=100, verbose_name=_('region'))
+    capacity = models.PositiveIntegerField(blank=True, verbose_name=_('capacity'))
+    meter = models.PositiveIntegerField(default=1, validators=[MaxValueValidator(100)], verbose_name=_('meter'))
+    base_value = models.PositiveIntegerField(verbose_name=_('base value'))
+    buy_assurance = models.BooleanField(default=False, verbose_name=_('buy insurance'))
+    guaranteed_gain = models.BooleanField(default=False, verbose_name=_('guaranteed gain'))
+    guaranteed_gain_percent = models.PositiveIntegerField(verbose_name=_('guaranteed gain percent'))
+    end_time = models.PositiveIntegerField(verbose_name=_('end time'))
+    description = models.TextField(max_length=500, verbose_name=_('description'))
+    status = models.CharField(max_length=10, choices=STATUS, verbose_name=_('status'))
     slug = models.SlugField(max_length=250, null=True, blank=True, unique=True, allow_unicode=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super(Case, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ('-datetime_created',)
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse('product_detail', args=[self.slug])
 
 
